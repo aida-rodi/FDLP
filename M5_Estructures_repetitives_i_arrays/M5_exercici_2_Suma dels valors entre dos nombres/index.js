7d414f48120daed44e51b5fcf86e823e1d2123f0
Aida <aida.rodi.1997@gmail.com>
2023-02-01 23:49:44 +0100

    something

--- M5_Estructures_repetitives_i_arrays/M5_exercici_2_Suma dels valors entre dos nombres/index.js
+++ M5_Estructures_repetitives_i_arrays/M5_exercici_2_Suma dels valors entre dos nombres/index.js
@@ -1,18 +1,29 @@
 function calculateIntervals() {
 
-    const number1 = Number(document.getElementById('userNumber1').value)
+    const number1 = Number(document.getElementById('userNumber1').value) // 9
 
-    const number2 = Number(document.getElementById('userNumber2').value)
+    const number2 = Number(document.getElementById('userNumber2').value) // 7
 
     const numbersList = []
 
     let result = 0
 
-    for (let index = number1; index <= number2; index++) {
+    if (number1 < number2) {
+        for (let index = number1; index <= number2; index++) {
 
-        result = result + index
+            result = result + index
+    
+            numbersList.push(index)
+        }
+    }
 
-        numbersList.push(index)
+    if (number1 > number2) {
+        for (let index = number1; index >= number2; index--) {
+
+            result = result + index
+
+            numbersList.push(index)
+        }
     }
 
     document.getElementById('result').innerHTML = `${numbersList.join(' + ')} = ${result}`
