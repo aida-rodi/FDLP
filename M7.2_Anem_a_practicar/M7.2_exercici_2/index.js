318477479caf6d6508ca7e4d45a32dfdf174bf15
Aida <aida.rodi.1997@gmail.com>
2023-02-07 15:58:34 +0100

    Finished M7.2_exercici_2

---
+++ M7.2_Anem_a_practicar/M7.2_exercici_2/index.js
@@ -0,0 +1,21 @@
+function drawTriangle() {
+    
+    const totalLines = Number(document.getElementById("userNumber").value)
+    let asterisk = ""
+    const triangle = []
+    
+
+    if (totalLines < 1 || isNaN(totalLines)) {
+        alert("Ep! Comprova que has introduït correctament el número de línies")
+        document.getElementById("result").innerHTML = ""
+    }
+
+    for (let lines = 1; lines <= totalLines; lines++) {
+        asterisk += "* "
+        triangle.push(asterisk)
+    }
+
+    const reverseTriangle = triangle.reverse()
+
+    document.getElementById("result").innerHTML = reverseTriangle.join("</br>")
+}
