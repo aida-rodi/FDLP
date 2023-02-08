162ba14e25cc757d3b54c535a8ab28b6214018ff
Aida <aida.rodi.1997@gmail.com>
2023-02-09 00:18:56 +0100

    Worked on M6_exercici_3

--- M6_Funcions/M6_exercici_3/index.js
+++ M6_Funcions/M6_exercici_3/index.js
@@ -1,30 +1,49 @@
-const time = []
+const time = [];
 
 function addDate() {
-    
-    const currentDate = new Date()
-    time.push(currentDate)
-    console.log(time)
-
+    const currentDate = new Date();
+    time.push(currentDate);
+    console.log(time);
 }
 
 function findNumberPosition(number, arrayOfNumbers) {
-    let result = -1
+    let result = -1;
     for (let position = 0; position < arrayOfNumbers.length; position++) {
         if (arrayOfNumbers[position] === number) {
-            result = position
+            result = position;
+            console.log(result);
         }
     }
-    return result
-    
+    return result;
 }
+
+function sentenceToLowerCase(userSentence) {
+    userSentence = userSentence.toLowerCase();
+    userSentence = userSentence.replaceAll(" ", "");
+    userSentence = userSentence.replaceAll(",", "");
+    userSentence = userSentence.replaceAll(".", "");
+    console.log(userSentence);
+}
+
+function findSchoolSubjects(arrayOfSubjects, subject) {}
 
 function menu(id) {
     if (id === 1) {
-        addDate()
+        addDate();
     }
     if (id === 2) {
-        const numberPosition = findNumberPosition(8, [9,52,1,47,85,8])
-        console.log(numberPosition)
+        findNumberPosition(8, [9, 52, 1, 47, 85, 8]);
+    }
+    if (id === 3) {
+        sentenceToLowerCase("A Cornellà, Tània i Aina tallen roca.");
+    }
+    if (id === 4) {
+        findSchoolSubjects([
+            "Història universal",
+            "Història d'Espanya",
+            "Geografia",
+            "Història de la llengua catalana",
+            "Antropologia",
+            "Anglès"], "Història");
     }
 }
