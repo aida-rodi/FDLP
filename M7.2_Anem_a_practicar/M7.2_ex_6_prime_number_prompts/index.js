90d2ec8549d0dd2904428804be94b97fee62dd2c
Aida <aida.rodi.1997@gmail.com>
2023-02-13 00:50:03 +0100

    M7.2_exercici_6

--- M7.2_Anem_a_practicar/M7.2_ex_6_prime_number_prompts/index.js
+++ M7.2_Anem_a_practicar/M7.2_ex_6_prime_number_prompts/index.js
@@ -1,8 +1,43 @@
-function calculate(){
+function calculate() {
+    let userNumber = undefined
+    let isPrime = undefined
+    
+    function checkPrime(userNumber) {
+        if (isNaN(userNumber) || userNumber <= 0) {
+            isPrime = undefined
+            checkPrime(userNumber = Number(prompt("Comprueba que hayas introducido un número mayor a 0.")))
+        }
+
+        for (let index = 2; index < userNumber; index++) {
+            if (userNumber % index === 0) {
+                isPrime = false
+            }
+    
+            else if (isPrime !== false) {
+                isPrime = true
+            }
+        }
+        
+        console.log (isPrime)
+        if (isPrime === true) {
+            document.getElementById("result").innerHTML = `El número ${userNumber} es primo.`
+        }
+        if (isPrime === false) {
+            isPrime = undefined
+            checkPrime(userNumber = Number(prompt("Inténtalo de nuevo")))
+        }
+    }
+
+    if (isPrime === undefined || isPrime === false) {
+        checkPrime(userNumber = Number(prompt("Escribe un número, por favor")));
+    }
+}
+
+
+/* function calculate(){
     let userNumber = Number(prompt("Escribe un número, por favor"));
 
     for (let index = 2; index < userNumber; index++) {
-        debugger
         if (userNumber%index === 0 ) {
             let isPrime = false  
             console.log (isPrime)
@@ -13,4 +48,4 @@ function calculate(){
     }
     console.log (isPrime)
     document.getElementById("result").innerHTML= `El numero ${userNumber} es primo`
-}
+} */
