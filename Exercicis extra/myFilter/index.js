7fa8e855552243ac5d750e16a92770129e3ab65a
Aida <aida.rodi.1997@gmail.com>
2023-02-15 23:22:46 +0100

    asdfg

---
+++ Exercicis extra/myFilter/index.js
@@ -0,0 +1,20 @@
+function myFilter(arrayOfNumbers, isBiggerThanFive) {}
+
+function isBiggerThanFive() {
+
+}
+
+
+function myFilter(arrayOfNumbers, isEvenNumber) {}
+
+function isEvenNumber() {}
+
+
+function myFilter(arrayOfNumbers, startsByB) {}
+
+function startsByB() {}
+
+
+function myFilter(arrayOfNumbers, isGrilloFamily) {}
+
+function isGrilloFamily() {}
